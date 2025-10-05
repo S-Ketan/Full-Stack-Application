6cb3cf0a149e6fecd0b5707dbda8ffbf00b21161
S-Ketan <ketan10923@gmail.com>
2025-10-05 03:31:54 +0200

    Implement user management features with CRUD operations and validation

--- frontend/src/views/Users.jsx
+++ frontend/src/views/Users.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosClient from "../axios-client";
 
 const Users = () => {
+  const [users, setUsers]= useState([]);
+  const [loading, setLoading]= useState(false);
+
+useEffect(()=>{},[])
+
+const getUsers=()=>{
+  setLoading(true);
+ axiosClient.get('/users')
+ .then(({data})=>{
+  console.log(data);
+ }).catch((err)=>{
+  console.log(err);
+  setLoading(false);
+ })
+}
+
   return (
     <div>Users</div>
   )
